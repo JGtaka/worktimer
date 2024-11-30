68026bd02f0d3494493020b767d4900ee2be36c9
taka <taka.email>
2024-11-30 15:49:01 +0900

    rails db:migrateを実行しtasksテーブルを作成

--- db/migrate/20241130055853_create_tasks.rb
+++ db/migrate/20241130055853_create_tasks.rb
@@ -1,7 +1,10 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-
+      t.references :users, foreign_key: true, null:false
+      t.string :name, null:false, defalut: ''
+      t.datetime :expired_at, null: false, defalut: ->{'NOW()'}
+      t.text :body, null:true
       t.timestamps
     end
   end
