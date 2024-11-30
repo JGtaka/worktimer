8336f4ddfa1b6cd0eeda69646214284a7fdced16
taka <taka.email>
2024-11-30 21:21:08 +0900

    tasksテーブルのusersをuserに変更

--- db/migrate/20241130055853_create_tasks.rb
+++ db/migrate/20241130055853_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-      t.references :users, foreign_key: true, null:false
+      t.references :user, foreign_key: true, null:false
       t.string :name, null:false, defalut: ''
       t.datetime :expired_at, null: false, defalut: ->{'NOW()'}
       t.text :body, null:true
--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,13 @@
 
 ActiveRecord::Schema[7.0].define(version: 2024_11_30_055853) do
   create_table "tasks", force: :cascade do |t|
-    t.integer "users_id", null: false
+    t.integer "user_id", null: false
     t.string "name", null: false
     t.datetime "expired_at", null: false
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["users_id"], name: "index_tasks_on_users_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,5 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_30_055853) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "tasks", "users", column: "users_id"
+  add_foreign_key "tasks", "users"
 end
