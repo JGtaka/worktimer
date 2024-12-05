cab7b223f28dd5f49cec9c964ce7ac7372d8b041
taka <taka.email>
2024-12-05 23:02:05 +0900

    詳細ページに一覧ページから遷移またはその逆を出来るようにした

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "/tasks", to: "tasks#index"
   get "/tasks/new", to: "tasks#new"
   post "/tasks", to: "tasks#create"
+  get "/tasks/:id", to: "tasks#show"
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
