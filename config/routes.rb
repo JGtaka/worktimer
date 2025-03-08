699e11a2f2c02f8cab9248de172964a561b6334b
taka <taka.email>
2025-03-08 15:44:04 +0900

    ルーティングでコメントアウトされていたものを削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,6 @@ Rails.application.routes.draw do
 
   resources :tasks
   root "top#index"
-   
-  #get "/tasks", to: "tasks#index"
-  #get "/tasks/new", to: "tasks#new"
-  #post "/tasks", to: "tasks#create"
-  #get "/tasks/:id", to: "tasks#show"
-  #get "/tasks/:id/edit", to: "tasks#edit", as: "edit_task"
-  #patch "/tasks/:id", to: "tasks#update" , as: "task"
-  #delete "/tasks/:id", to: "tasks#destroy" 
-  
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
