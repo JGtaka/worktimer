0d4ca05cb79ee2539d0636a961bb258b293f91f3
taka <taka.email>
2025-03-08 14:37:29 +0900

    レビューを修正、buildメソッドを使用

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,8 +15,10 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-    @task.user_id = current_user.id
+    #@task = Task.new(task_params)
+    #@task.user_id = current_user.id
+    @task = current_user.tasks.build(task_params)
+
     if @task.save
      redirect_to task_path(@task)
     else
