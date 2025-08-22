279800a010a72777f2c861566e9ff8d430aabe82
taka <taka.email>
2025-08-22 20:42:14 +0900

    MVCの責務分離を反映

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,6 +14,7 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new
+    @priority_options = Task.priorities.keys.map { |p| [p.humanize, p] }
   end
 
   def create
@@ -27,6 +28,8 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
+     @priority_options = Task.priorities.keys.map { |p| [p.humanize, p] }
   end
 
   def update
